1042c57ed742cb7b8d82e793b706ea80a2c13335
Donpot <patricktec19938@live.dk>
2016-05-31 13:04:58 +0200

    Database Done

---
+++ Novelle Hjemmeside/Controllers/NovelleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Novelle_Hjemmeside.Controllers
+{
+    public class NovelleController : Controller
+    {
+        // GET: Novelle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
