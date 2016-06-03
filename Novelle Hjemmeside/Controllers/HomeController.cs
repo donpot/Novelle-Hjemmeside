6c6472905b1cbe73a7c6be01c449530995edd939
Donpot <patricktec19938@live.dk>
2016-06-03 13:19:06 +0200

    shit

--- Novelle Hjemmeside/Controllers/HomeController.cs
+++ Novelle Hjemmeside/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Novelle_Hjemmeside.Classes;
 
 namespace Novelle_Hjemmeside.Controllers
 {
@@ -11,6 +12,7 @@ namespace Novelle_Hjemmeside.Controllers
         // GET: Home
         public ActionResult Index()
         {
+
             return View();
         }
     }
