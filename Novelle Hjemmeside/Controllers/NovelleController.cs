9e9b6c0bab8aed920c35edd48e23b5d18ec2fe76
user124 <c-engelund@hotmail.com>
2016-06-07 10:51:29 +0200

    opretnovelle

--- Novelle Hjemmeside/Controllers/NovelleController.cs
+++ Novelle Hjemmeside/Controllers/NovelleController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Novelle_Hjemmeside.Classes;
+using Novelle_Hjemmeside.Models;
 
 namespace Novelle_Hjemmeside.Controllers
 {
@@ -11,6 +13,32 @@ namespace Novelle_Hjemmeside.Controllers
         // GET: Novelle
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpGet]
+        [ActionName("OpretNovelle")]
+        public ActionResult OpretNovelle_Get()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("OpretNovelle")]
+        public ActionResult OpretNovelle_Post(NovelleModel addNovel)
+        {
+            Database addNovelDb = new Database();
+            UserModel addNovelUser = new UserModel();
+
+            addNovelUser = (UserModel)Session["login"];
+            if (ModelState.IsValid)
+            {
+                addNovelDb.TilføjNovelle(
+                    addNovelUser.UserName,
+                    addNovelUser.User_ID,
+                    addNovel.NovelleNavn,
+                    addNovel.NovelleBeskrivelse);
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
     }
